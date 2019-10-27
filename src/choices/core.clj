1be10a27609e87e19a43aac4983aa33701812eba
John Flinchbaugh <john@hjsoft.com>
2019-10-26 23:21:32 -0400

    fn to map choice to people who will showup

--- src/choices/core.clj
+++ src/choices/core.clj
@@ -15,3 +15,13 @@
     reverse
     (map first)))
 
+(defn choices-to-people [people]
+  (->>
+    people
+    (map (juxt last first))
+    (reduce
+      (fn [m [k v]] (apply conj m (for [kk k] [kk v])))
+      [])
+    (reduce (fn [m [k v]] (conj m {k [v]})) [])
+    (apply merge-with concat)
+    ))
--- test/choices/core_test.clj
+++ test/choices/core_test.clj
@@ -4,31 +4,41 @@
 
 (deftest test-person-to-scores
   (testing "person-to-scores"
-   (is (=
-        {"c1" 1.0
-         "c2" 1.0}
-        (person-to-scores ["name" 1.0 ["c1" "c2"]])))
-   (is (=
-        {"c1" 0.5
-         "c2" 0.5}
-        (person-to-scores ["name" 0.5 ["c1" "c2"]])))))
+    (is (=
+         {"c1" 1.0
+          "c2" 1.0}
+         (person-to-scores ["name" 1.0 ["c1" "c2"]])))
+    (is (=
+         {"c1" 0.5
+          "c2" 0.5}
+         (person-to-scores ["name" 0.5 ["c1" "c2"]])))))
 
 (deftest test-tally
   (testing "tally"
-   (is (= {"c1" 1.5, "c2" 1.0, "c3" 0.5}
-          (tally [{"c1" 1.0, "c2" 1.0} {"c1" 0.5, "c3" 0.5}])))))
+    (is (=
+         {"c1" 1.5, "c2" 1.0, "c3" 0.5}
+         (tally [{"c1" 1.0, "c2" 1.0} {"c1" 0.5, "c3" 0.5}])))))
 
 (deftest test-rank-choices
   (testing "rank-choices"
-    (is (= ["c1" "c2" "c3"]
-          (rank-choices {"c2" 1.0, "c3" 0.5, "c1" 1.5})))))
+    (is (=
+         ["c1" "c2" "c3"]
+         (rank-choices {"c2" 1.0, "c3" 0.5, "c1" 1.5})))))
+
+(deftest test-choices-to-people
+  (testing "choices to people"
+    (is (=
+         {"c1" ["p1"], "c2" ["p1" "p2"], "c3" ["p2"]}
+         (choices-to-people
+          [["p1" 1.0 ["c1" "c2"]]
+           ["p2" 1.5 ["c2" "c3"]]])))))
 
 (deftest test-integrated
-  (testing "person to choices"
-    (is (= ["c2" "c3" "c1"]
-          (->> [
-                ["p1" 1.0 ["c1" "c2"]]
-                ["p2" 1.5 ["c2" "c3"]]]
-            (map person-to-scores)
-            tally
-            rank-choices)))))
+  (testing "person to ranked choices"
+    (is (=
+         ["c2" "c3" "c1"]
+         (->> [["p1" 1.0 ["c1" "c2"]]
+               ["p2" 1.5 ["c2" "c3"]]]
+              (map person-to-scores)
+              tally
+              rank-choices)))))
