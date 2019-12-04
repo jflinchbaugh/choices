4bad4f39ab1e5b1f9307ac22801283be0f41ab2c
John Flinchbaugh <john@hjsoft.com>
2019-12-03 22:43:42 -0500

    introduce more web stuff
    used:
    * hiccup
    
    unused to this point:
    * cheshire
    * compojure
    
    not working at the moment:
    * liberator

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.7.1"]]
+                 [ring "1.7.1"]
+                 [hiccup "1.0.5"]
+                 [cheshire "5.9.0"]
+                 [compojure "1.6.1"]
+                 [liberator "0.15.3"]]
   :plugins [[lein-ring "0.12.5"]]
 
   :repl-options {:init-ns choices.core}
--- src/choices/core.clj
+++ src/choices/core.clj
@@ -1,15 +1,16 @@
 (ns choices.core
-(:require [clojure.set :as set]))
-
-(defn handler [request]
-  {:status 200
-   :headers {"Content-type" "text/plain"}
-   :body "Hello world."})
+  (:require
+   [clojure.set :as set]
+   [hiccup.core :as h]
+   [cheshire.core :as ch]
+   [compojure.core :refer [defroutes ANY GET]]
+   [ring.middleware.params :refer [wrap-params]]
+   [liberator.core :refer [resource defresource] :as r]))
 
 (defn person-to-scores
   "given a preson structure, produce their scores per choice"
   [[name score choices]]
-  (into {} (for [c choices] [c score]))) 
+  (into {} (for [c choices] [c score])))
 
 (defn tally
   "total up scores per choice"
@@ -20,38 +21,84 @@
   "given choices with scores, produce ranked choices"
   [choice-scores]
   (->>
-    choice-scores
-    (into [])
-    (sort-by second)
-    reverse
-    (map first)))
+   choice-scores
+   (into [])
+   (sort-by second)
+   reverse
+   (map first)))
 
 (defn distribute-keys
   "[[y z] x] -> [{y x} {z x}]"
   [lst]
   (reduce
-    (fn [m [k v]] (apply conj m (for [kk k] {kk #{v}})))
-    []
-    lst)
-  )
+   (fn [m [k v]] (apply conj m (for [kk k] {kk #{v}})))
+   []
+   lst))
 
 (defn choices-to-people
   "given a list of people structures, produce a map of choice to names "
   [people]
   (->>
-    people
-    (map (juxt last first))
-    distribute-keys
-    (apply merge-with (comp set concat))
-    ))
+   people
+   (map (juxt last first))
+   distribute-keys
+   (apply merge-with (comp set concat))))
 
 (defn rank-choices-with-people
   "transform list of people and choices to ranked choices and people"
   [people]
   (let [ranked-choices (->>
-                         people
-                         (map person-to-scores)
-                         tally
-                         rank-choices)
+                        people
+                        (map person-to-scores)
+                        tally
+                        rank-choices)
         choices-people (choices-to-people people)]
     (map (juxt identity choices-people) ranked-choices)))
+
+(defn do-home [request]
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body (h/html
+           [:head
+            [:link {:rel "stylesheet" :href "/style.css"}]]
+           [:body
+            [:h1 "home World"]])})
+
+(defn html-with-head [body]
+  (h/html
+   [:head
+    [:link {:rel "stylesheet" :href "/style.css"}]]))
+
+(defn do-html [request]
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body (h/html
+          [:head
+           [:link {:rel "stylesheet" :href "/style.css"}]]
+          [:body
+           [:h1 "an h1"]
+           [:table
+            [:tbody
+             (for [p request]
+               [:tr
+                [:td (key p)]
+                [:td (val p)]])]]])})
+
+(defn do-style [request]
+  {:status 200
+   :headers {"Content-type" "text/css"}
+   :body "html, body { color: white; background-color: black;}"})
+
+(defn not-found [request]
+  {:status 404
+   :body "Not Found"})
+
+(defn handler [request]
+  (cond
+    (re-matches #"/style.css" (:uri request)) (do-style request)
+    (re-matches #"/html.*" (:uri request)) (do-html request)
+    (re-matches #"/" (:uri request)) (do-home request)
+    (re-matches #"/resource" (:uri request)) (resource
+                                               :available-media-types ["text/plain"]
+                                              :handle-ok "hello resource")
+    :else (not-found request)))
